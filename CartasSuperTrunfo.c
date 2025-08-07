b6ec75f6f20043bbcfe193f458f9d86d864730b2
Igor Souto <igorpereira-111@hotmail.com>
2025-08-07 22:40:53 +0000

    Criei a solicitação de dados e armazenamento para carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,4 +17,30 @@ int main(){
     float pib2;
     int pontos_turisticos1;
     int pontos_turisticos2;
+
+ printf("Digite a carta: ");
+    scanf(" %c", &carta1);
+
+    printf("Digite o estado (ex: SP): ");
+    scanf("%2s", estado1);
+
+    printf("Digite o código da carta (ex: A12): ");
+    scanf("%3s", codigo1);
+
+    printf("Digite o nome da cidade: ");
+    scanf(" %20[^\n]", nome_da_cidade1);
+
+    printf("Digite a população: ");
+    scanf("%d", &populacao1);
+
+    printf("Digite a área em km²: ");
+    scanf("%f", &area1);
+
+    printf("Digite o PIB: ");
+    scanf("%f", &pib1);
+
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%d", &pontos_turisticos1);
+
+
 }
